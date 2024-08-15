e335f0d29817b3de14e98192c513db405c7f56fc
Alfred Dobradi <alfreddobradi@gmail.com>
2024-08-15 22:53:42 +0100

    fix: Remove NATS encoded connection

--- transport/transport.go
+++ transport/transport.go
@@ -1,33 +1,21 @@
 package transport
 
 import (
-	"fmt"
-
 	"github.com/nats-io/nats.go"
-	"github.com/nats-io/nats.go/encoders/protobuf"
-)
-
-type Encoder string
-
-const (
-	EncoderJSON     Encoder = "json"
-	EncoderProtobuf Encoder = "protobuf"
 )
 
 type Configuration struct {
 	URL     string
-	Encoder Encoder
 	Options []nats.Option
 }
 
 var DefaultConfig = Configuration{
 	URL:     "127.0.0.1:4222",
-	Encoder: EncoderJSON,
 	Options: make([]nats.Option, 0),
 }
 
 type Connection struct {
-	*nats.EncodedConn
+	*nats.Conn
 }
 
 func NewEncodedConn(config Configuration) (*Connection, error) {
@@ -36,31 +24,5 @@ func NewEncodedConn(config Configuration) (*Connection, error) {
 		return nil, err
 	}
 
-	var encoder string
-	switch config.Encoder {
-	case EncoderJSON:
-		encoder = nats.JSON_ENCODER
-	case EncoderProtobuf:
-		encoder = protobuf.PROTOBUF_ENCODER
-	default:
-		return nil, fmt.Errorf("invalid encoder: %s", config.Encoder)
-	}
-
-	nc, err := nats.NewEncodedConn(c, encoder)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Connection{nc}, nil
-}
-
-func ParseEncoder(str string) Encoder {
-	encoder := EncoderJSON
-	switch str {
-	case "json":
-	case "protobuf":
-		encoder = EncoderProtobuf
-	}
-
-	return encoder
+	return &Connection{c}, nil
 }
